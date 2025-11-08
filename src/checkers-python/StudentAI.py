3a4e6ed13df3c8777b8a30712aeed80e095b0d2b
William Lay <laywc@uci.edu>
2025-11-08 14:46:50 -0800

    added first draft of simple heuristics for random AI

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -20,13 +20,75 @@ class StudentAI():
         else:
             self.color = 1
         moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
-        self.board.make_move(move,self.color)
-        return move
+
+        best_move = moves[0][0]
+        best_util = self.get_util(move)
+
+        for i in range(3):
+            index = randint(0,len(moves)-1)
+            inner_index =  randint(0,len(moves[index])-1)
+            move = moves[index][inner_index]
+            if move == best_move:
+                continue
+            util = self.get_util(move)
+            if util > best_util:
+                best_move = move
+                best_util = util
+
+        self.board.make_move(best_move,self.color)
+        return best_move
 
     class Node:
         def __init__(self):
             self.w = 0
             self.s = 0
+
+    def get_util(self, move: Move) -> int:
+        util: int = 0
+        util += self.util_num_captures(move)
+        util += self.util_back_row(move)
+        util += self.util_king(move)
+        util += self.util_towards_center(move)
+        util += self.util_enemy_capture(move)
+        return util
+
+    def util_num_captures(self, move: Move) -> int:
+        l: int = len(move)
+        return l * 3 if l > 2 else 0
+
+    def util_back_row(self, move: Move) -> int:
+        if len(move) == 0:
+            return 0
+        return -5 if move[0][1] == 0 else 0
+
+    def util_king(self, move: Move) -> int:
+        if len(move) == 0:
+            return 0
+        return 5 if move[-1][1] == self.row - 1 else 0
+
+    def util_towards_center(self, move: Move) -> int:
+        if len(move) == 0:
+            return 0
+        final_x: int = move[-1][0]
+        if 0 == final_x or final_x == self.col - 1:
+            return -2 * self.col
+        return 0
+        # center: int = (self.col - 1) / 2
+        # distance_from_center: int = abs(center - final_x)
+        # return -5 * distance_from_center
+
+    def util_enemy_capture(self, move: Move):
+        if len(move) == 0:
+            return 0
+        pos_x = move[-1][0]
+        pos_y = move[-1][1]
+
+        if self.board.is_in_board(pos_x-1, pos_y+1):
+            left = self.board.board[pos_x-1][pos_y+1]
+            if left.get_color().lower() == "w":
+                return -100
+        if self.board.is_in_board(pos_x+1, pos_y+1):
+            right = self.board.board[pos_x+1][pos_y+1]
+            if right.get_color().lower() == "w":
+                return -100
+        return 0
