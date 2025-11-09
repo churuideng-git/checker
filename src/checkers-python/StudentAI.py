81a20f90308750d7be254cbae4948c1ddb200832
William Lay <laywc@uci.edu>
2025-11-08 18:08:52 -0800

    rewrote and fixed heuristics

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -22,18 +22,29 @@ class StudentAI():
         moves = self.board.get_all_possible_moves(self.color)
 
         best_move = moves[0][0]
-        best_util = self.get_util(move)
+        best_util = self.get_util(best_move)
 
-        for i in range(3):
-            index = randint(0,len(moves)-1)
-            inner_index =  randint(0,len(moves[index])-1)
-            move = moves[index][inner_index]
-            if move == best_move:
-                continue
-            util = self.get_util(move)
-            if util > best_util:
-                best_move = move
-                best_util = util
+        # pick 3
+        # for i in range(3):
+        #     index = randint(0,len(moves)-1)
+        #     inner_index =  randint(0,len(moves[index])-1)
+        #     move = moves[index][inner_index]
+        #     if move == best_move:
+        #         continue
+        #     util = self.get_util(move)
+        #     if util > best_util:
+        #         best_move = move
+        #         best_util = util
+
+        # analyze all
+        for piece in moves:
+            for move in piece:
+                if move == best_move:
+                    continue
+                util = self.get_util(move)
+                if util > best_util:
+                    best_move = move
+                    best_util = util
 
         self.board.make_move(best_move,self.color)
         return best_move
@@ -44,51 +55,46 @@ class StudentAI():
             self.s = 0
 
     def get_util(self, move: Move) -> int:
+        if len(move) == 0:
+            return 0
+
         util: int = 0
-        util += self.util_num_captures(move)
         util += self.util_back_row(move)
         util += self.util_king(move)
         util += self.util_towards_center(move)
         util += self.util_enemy_capture(move)
         return util
 
-    def util_num_captures(self, move: Move) -> int:
-        l: int = len(move)
-        return l * 3 if l > 2 else 0
-
     def util_back_row(self, move: Move) -> int:
-        if len(move) == 0:
-            return 0
-        return -5 if move[0][1] == 0 else 0
+        start_row = move[0][0]
+        return -2 * self.row if start_row == 0 else 0
 
     def util_king(self, move: Move) -> int:
-        if len(move) == 0:
-            return 0
-        return 5 if move[-1][1] == self.row - 1 else 0
+        final_row: int = move[-1][0]
+        final_col: int = move[-1][1]
+        return self.row if final_row == self.row - 1 and not self.board.board[final_row][final_col].is_king else 0
 
     def util_towards_center(self, move: Move) -> int:
-        if len(move) == 0:
-            return 0
-        final_x: int = move[-1][0]
-        if 0 == final_x or final_x == self.col - 1:
-            return -2 * self.col
-        return 0
-        # center: int = (self.col - 1) / 2
-        # distance_from_center: int = abs(center - final_x)
-        # return -5 * distance_from_center
+        final_col: int = move[-1][1]
+        center: int = (self.col - 1) / 2
+        distance_from_center: int = center - final_col
+        return -1 * distance_from_center * distance_from_center
 
     def util_enemy_capture(self, move: Move):
-        if len(move) == 0:
-            return 0
-        pos_x = move[-1][0]
-        pos_y = move[-1][1]
+        piece_color = {1:"B", 2:"W"}
+        own_color = piece_color[self.color]
+        opp_color = piece_color[self.opponent[self.color]]
 
-        if self.board.is_in_board(pos_x-1, pos_y+1):
-            left = self.board.board[pos_x-1][pos_y+1]
-            if left.get_color().lower() == "w":
-                return -100
-        if self.board.is_in_board(pos_x+1, pos_y+1):
-            right = self.board.board[pos_x+1][pos_y+1]
-            if right.get_color().lower() == "w":
-                return -100
-        return 0
+        row = move[-1][0]
+        col = move[-1][1]
+        front = row + (1 if self.color == 1 else -1)
+
+        left = self.board.board[front][col-1].get_color() if self.board.is_in_board(front, col-1) else ""
+        right = self.board.board[front][col+1].get_color() if self.board.is_in_board(front, col+1) else ""
+
+        util = 0
+        if left == own_color or right == own_color:
+            util += 5
+        if left == opp_color or right == opp_color:
+            util += -10
+        return util
